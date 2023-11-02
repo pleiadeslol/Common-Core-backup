cf4db16669e0fee6588114ef5fe3d599f0654898
rzarhoun <rzarhoun@c4r3p8.1337.ma>
2023-11-02 15:47:03 +0100

    memcpy func

---
+++ Libft/ft_memcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rzarhoun <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/02 14:23:14 by rzarhoun          #+#    #+#             */
+/*   Updated: 2023/11/02 14:46:53 by rzarhoun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+	char	*c_dest;
+	char	*c_src;
+
+	i = 0;
+	c_dest = (char *)dest;
+	c_src = (char *)src;
+	if (dest == NULL )
+		return (NULL);
+	while (i < n)
+	{
+		c_dest[i] = c_src[i];
+		i++;
+	}
+	return (dest);
+}
