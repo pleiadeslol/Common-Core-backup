83e40ab6bb2c0c0dd83223190026e8656361c6e6
rzarhoun <rzarhoun@c4r3p8.1337.ma>
2023-11-02 16:17:41 +0100

    memmpve func

---
+++ Libft/ft_memmove.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rzarhoun <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/02 15:47:23 by rzarhoun          #+#    #+#             */
+/*   Updated: 2023/11/02 16:10:20 by rzarhoun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*memmove(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (src < dest)
+	{
+		i = n;
+		while (i > 0)
+		{
+			i--;
+			((char *)dest)[i] = ((char *)src)[i];
+		}
+	}
+	else
+	{
+		i = 0;
+		while (i < n)
+		{
+			((char *)dest)[i] = ((char *)src)[i];
+			i++;
+		}
+	}
+	return (dest);
+}
