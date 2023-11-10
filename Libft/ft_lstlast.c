4bf2f13341205171535f6ca8bad901297a1ea34e
rzarhoun <rzarhoun@c4r3p5.1337.ma>
2023-11-10 14:59:54 +0100

    help

--- Libft/ft_lstlast.c
+++ Libft/ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: rzarhoun <rzarhoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 00:28:35 by rzarhoun          #+#    #+#             */
-/*   Updated: 2023/11/09 08:37:38 by rzarhoun         ###   ########.fr       */
+/*   Updated: 2023/11/10 14:57:50 by rzarhoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ t_list	*ft_lstlast(t_list *lst)
 	last = lst;
 	if (last == NULL)
 		return (NULL);
-	while (last)
+	while (last->next)
 		last = last->next;
 	return (last);
 }
