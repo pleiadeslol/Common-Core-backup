ccc765dc470854ea9627bb3d1da8b3baa5db18b7
rzarhoun <rzarhoun@c4r3p7.1337.ma>
2023-11-10 23:36:57 +0100

    fixing more errors

--- Libft/ft_strncmp.c
+++ Libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: rzarhoun <rzarhoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 11:56:19 by rzarhoun          #+#    #+#             */
-/*   Updated: 2023/11/10 23:27:40 by rzarhoun         ###   ########.fr       */
+/*   Updated: 2023/11/10 23:35:19 by rzarhoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,11 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
+	if (!n)
+		return (0);
 	while (s1[i] - s2[i] == 0 && s1[i] && s2[i] && i < n)
 		i++;
-	return (s1[i] - s2[i]);
+	if (i == n)
+		return (0);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
