c246e21b4d0e4125ac514e5a9a7dcbdd91a6ee00
rzarhoun <rzarhoun@c4r3p3.1337.ma>
2023-11-11 15:57:44 +0100

    fixing errors

--- Libft/ft_atoi.c
+++ Libft/ft_atoi.c
@@ -6,33 +6,49 @@
 /*   By: rzarhoun <rzarhoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 12:22:24 by rzarhoun          #+#    #+#             */
-/*   Updated: 2023/11/11 13:53:30 by rzarhoun         ###   ########.fr       */
+/*   Updated: 2023/11/11 15:56:10 by rzarhoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static void	handle_sign(const char *str, int *i, int *j, int *sign)
+{
+	while (str[*i] == ' ' || (str[*i] >= 9 && str[*i] <= 13))
+		(*i)++;
+	if (str[*i] == '-' || str[*i] == '+')
+	{
+		if (str[(*i)++] == '-')
+		{
+			*sign = -1;
+		}
+		else
+		{
+			*sign = 1;
+		}
+		(*j)++;
+	}
+}
+
 int	ft_atoi(const char *str)
 {
 	int	i;
+	int	j;
 	int	sign;
 	int	res;
 
 	i = 0;
+	j = 0;
 	sign = 1;
 	res = 0;
-	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
-		i++;
-	while (str[i] == '-' || str[i] == '+')
-	{
-		if (str[i] == '-')
-			sign *= -1;
-		i++;
-	}
+	handle_sign(str, &i, &j, &sign);
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		res = (str[i] - '0') + (res * 10);
 		i++;
 	}
-	return (res * sign);
+	if (j > 1)
+		return (0);
+	else
+		return (res * sign);
 }
--- Libft/ft_memmove.c
+++ Libft/ft_memmove.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rzarhoun <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: rzarhoun <rzarhoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 15:47:23 by rzarhoun          #+#    #+#             */
-/*   Updated: 2023/11/02 16:10:20 by rzarhoun         ###   ########.fr       */
+/*   Updated: 2023/11/11 15:14:33 by rzarhoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*memmove(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	size_t	i;
 
