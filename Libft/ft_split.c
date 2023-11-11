577bae9e803db3c1683a1b30cd6c810aa71a8f4f
rzarhoun <rzarhoun@c4r3p3.1337.ma>
2023-11-11 20:28:30 +0100

    DONE

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -6,40 +6,75 @@
 /*   By: rzarhoun <rzarhoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 18:44:31 by rzarhoun          #+#    #+#             */
-/*   Updated: 2023/11/06 23:03:57 by rzarhoun         ###   ########.fr       */
+/*   Updated: 2023/11/11 20:27:20 by rzarhoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	count_words_str(const char *s, char c)
+static int	count_words_str(const char *s, char c)
 {
-	size_t	count;
-	int		i;
+	int		count;
+	size_t	i;
 
 	count = 0;
 	i = 0;
 	while (s[i])
 	{
-		if (s[i] != c && (s[i + 1] == c || !s[i + 1]))
+		if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
 			count++;
 		i++;
 	}
-	count++;
 	return (count);
+}
+
+static char	*fill_words(char const *s, char c)
+{
+	char	*str;
+	size_t	i;
+	size_t	len;
+
+	i = 0;
+	len = 0;
+	while (s[len] && s[len] != c)
+		len++;
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	while (s[i] && i < len)
+	{
+		str[i] = s[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	int		i;
-	size_t	count;
+	size_t	i;
 	char	**str_f;
 
 	i = 0;
-	if (s == NULL)
+	if (!s)
 		return (NULL);
-	count = count_words_str(s, c);
-	str_f = (char **)malloc(sizeof(char) * (count + 1));
+	str_f = (char **)malloc(sizeof(char *) * (count_words_str(s, c) + 1));
 	if (str_f == NULL)
 		return (NULL);
+	while (*s)
+	{
+		while (*s && *s == c)
+			s++;
+		if (!*s)
+			break ;
+		str_f[i] = fill_words(s, c);
+		if (!str_f[i])
+		{
+			free(str_f);
+			return (NULL);
+		}
+		s += ft_strlen(str_f[i++]);
+	}
+	str_f[i] = 0;
+	return (str_f);
 }
