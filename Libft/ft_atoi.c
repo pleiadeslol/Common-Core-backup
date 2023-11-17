d33c88450b91db85118412d6cadd12819682d8d1
rzarhoun <rzarhoun@c4r3p7.1337.ma>
2023-11-17 13:48:18 +0100

    v

--- Libft/ft_atoi.c
+++ Libft/ft_atoi.c
@@ -6,13 +6,13 @@
 /*   By: rzarhoun <rzarhoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 12:22:24 by rzarhoun          #+#    #+#             */
-/*   Updated: 2023/11/15 16:23:22 by rzarhoun         ###   ########.fr       */
+/*   Updated: 2023/11/17 11:30:01 by rzarhoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	handle_sign(const char *str, int *i, int *j, int *sign)
+/**static void	handle_sign(const char *str, int *i, int *j, int *sign)
 {
 	while (str[*i] == ' ' || (str[*i] >= 9 && str[*i] <= 13))
 		(*i)++;
@@ -28,27 +28,33 @@ static void	handle_sign(const char *str, int *i, int *j, int *sign)
 		}
 		(*j)++;
 	}
-}
+}**/
 
 int	ft_atoi(const char *str)
 {
 	int			i;
-	int			j;
 	int			sign;
 	long long	res;
 
 	i = 0;
-	j = 0;
 	sign = 1;
 	res = 0;
-	handle_sign(str, &i, &j, &sign);
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+		if (str[i++] == '-')
+			sign = -1;
 	while (str[i] >= '0' && str[i] <= '9')
 	{
+		if (res > 9223372036854775807 / 10)
+		{
+			if (sign > 0)
+				return (-1);
+			else
+				return (0);
+		}
 		res = (str[i] - '0') + (res * 10);
 		i++;
 	}
-	if (j > 1)
-		return (0);
-	else
-		return (res * sign);
+	return (res * sign);
 }
