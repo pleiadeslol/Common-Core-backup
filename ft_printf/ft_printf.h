4ba8dc1896b0a4273ff7e9405bb9af3a83e1f5d4
rzarhoun <rzarhoun@c3r10p6.1337.ma>
2023-11-22 17:03:12 +0100

    added files

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: rzarhoun <rzarhoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 21:30:32 by rzarhoun          #+#    #+#             */
-/*   Updated: 2023/11/22 15:50:54 by rzarhoun         ###   ########.fr       */
+/*   Updated: 2023/11/22 16:01:06 by rzarhoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 # include <unistd.h>
 # include <stdarg.h>
 
-int		ft_printf(const char *, ...);
+int		ft_printf(const char *str, ...);
 void	ft_putchar(char c);
 void	ft_putstr(char *str);
 
--- ft_printf/ft_putnbr_base.c
+++ ft_printf/ft_putnbr_base.c
@@ -1,37 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printvoidptr.c                                  :+:      :+:    :+:   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rzarhoun <rzarhoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/21 23:38:34 by rzarhoun          #+#    #+#             */
-/*   Updated: 2023/11/22 16:16:58 by rzarhoun         ###   ########.fr       */
+/*   Created: 2023/11/22 16:21:23 by rzarhoun          #+#    #+#             */
+/*   Updated: 2023/11/22 17:01:55 by rzarhoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include "ft_printf.h"
 
-void	ft_print_void_ptr(void *p)
+void    ft_putnbr_base()
 {
-    unsigned char   tmp;
-    char *str;
     
-    if (p = 0)
-        ft_putstr("(nil)");
-    else if (p <= 15)
-    {
-        ft_putstr("0x");
-        ft_print_hex((int)p);
-    }
-}
-
-#include <stdio.h>
-
-int main()
-{
-    void *p;
-    for (p = 0; p < 20; p++)
-        printf("%p\n", p);
-    return 0;
 }
