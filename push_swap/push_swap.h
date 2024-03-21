91bc6fef77a03b1fa3af918baef25aeac377a09b
rzarhoun <rzarhoun@e2r10p4.1337.ma>
2024-03-21 06:56:21 +0000

    added function to header

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -48,5 +48,17 @@ void	ft_swap(t_stack **s);
 void	ft_push(t_stack **a, t_stack **b);
 void	ft_rotate(t_stack **s);
 void	ft_reverse_rotate(t_stack **s);
+//rules functions
+void	sa(t_stack **a);
+void	sb(t_stack **b);
+void	ss(t_stack **a, t_stack **b);
+void	pa(t_stack **a, t_stack **b);
+void	pb(t_stack **a, t_stack **b);
+void	ra(t_stack **a);
+void	rb(t_stack **b);
+void	rr(t_stack **a, t_stack **b);
+void	rra(t_stack **a);
+void	rrb(t_stack **b);
+void	rrr(t_stack **a, t_stack **b);
 
 #endif
