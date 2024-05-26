3c8b11d7a0e9493b068782ce9156ee09abecdaf7
rzarhoun <rzarhoun@e2r9p4.1337.ma>
2024-05-27 00:04:25 +0100

    bonus

--- pipex/bonus/exec_cmd.c
+++ pipex/bonus/exec_cmd.c
@@ -14,16 +14,23 @@
 
 void	exec_cmd(t_args *args, char **envp)
 {
-	int	pipe_fd[args->count - 1][2];
-	int	i;
-	int	j;
+	int		pipe_fd[args->count - 1][2];
+	int		i;
+	int		j;
 	pid_t	id;
 
+	// i = 0;
+	// pipe_fd = malloc(sizeof(int *) * (args->count - 1));
+	// while (i < args->count)
+	// {
+	// 	pipe_fd[i] = malloc(sizeof(int) * 2);
+	// 	i++;
+	// }
 	i = 0;
 	while (i < args->count)
 	{
 		j = 0;
-		if ( i < args->count - 1)
+		if (i < args->count - 1)
 		{
 			if (pipe(pipe_fd[i]) == -1)
 				exit(1);
@@ -36,21 +43,17 @@ void	exec_cmd(t_args *args, char **envp)
 				dup2(args->fd1, STDIN_FILENO);
 				close(args->fd1);
 				dup2(pipe_fd[i][1], STDOUT_FILENO);
-				close(pipe_fd[i][1]);
 			}
 			else if (i == args->count - 1)
 			{
 				dup2(pipe_fd[i - 1][0], STDIN_FILENO);
-				close(pipe_fd[i - 1][0]);
 				dup2(args->fd2, STDOUT_FILENO);
 				close(args->fd2);
 			}
 			else
 			{
 				dup2(pipe_fd[i - 1][0], STDIN_FILENO);
-				close(pipe_fd[i - 1][0]);
 				dup2(pipe_fd[i][1], STDOUT_FILENO);
-				close(pipe_fd[i][1]);
 			}
 			while (j < args->count - 1)
 			{
@@ -60,9 +63,8 @@ void	exec_cmd(t_args *args, char **envp)
 			}
 			close(args->fd1);
 			close(args->fd2);
-			execve(args->path[i], args->cmd[i], envp);
-			// if (execve(args->path[i], args->cmd[i], envp) < 0)
-			// 	exit(1);
+			if (execve(args->path[i], args->cmd[i], envp) < 0)
+				exit(1);
 		}
 		i++;
 	}
@@ -75,6 +77,6 @@ void	exec_cmd(t_args *args, char **envp)
 	}
 	close(args->fd1);
 	close(args->fd2);
-	while(wait(NULL) > 0)
+	while (wait(NULL) > 0)
 		;
 }
