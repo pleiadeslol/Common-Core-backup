c0a7889149fb952505bab4749f4e938bc8707969
rzarhoun <rzarhoun@c4r9p3.1337.ma>
2024-07-13 10:48:33 +0100

    microshell by yacoub

--- minishell/main.c
+++ minishell/main.c
@@ -1,16 +1,54 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: rzarhoun <rzarhoun@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/07 01:56:58 by rzarhoun          #+#    #+#             */
-/*   Updated: 2024/06/07 01:57:32 by rzarhoun         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
-int	main(int ac, char **av)
+int main(int ac, char **av, char **envp)
 {
-	
+    int i;
+    char **envp_copy;
+    char *new_prompt = "PS1=minishell$ ";
+
+    for (i = 0; envp[i] != NULL; i++);
+
+    envp_copy = (char **)malloc(sizeof(char *) * (i + 2));
+    if (envp_copy == NULL) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+
+    int envp_copy_index = 0;
+    for (int j = 0; envp[j] != NULL; j++)
+    {
+        if (strncmp("PS1=", envp[j], 4) != 0)
+        {
+            envp_copy[envp_copy_index] = strdup(envp[j]);
+            if (envp_copy[envp_copy_index] == NULL)
+            {
+                perror("strdup");
+                exit(EXIT_FAILURE);
+            }
+            envp_copy_index++;
+        }
+    }
+
+    envp_copy[envp_copy_index] = strdup(new_prompt);
+    if (envp_copy[envp_copy_index] == NULL)
+    {
+        perror("strdup");
+        exit(EXIT_FAILURE);
+    }
+    envp_copy[++envp_copy_index] = NULL;
+
+    char *cmds[] = { "bash", "--norc", "-i", NULL };
+
+    execve("/bin/bash", cmds, envp_copy);
+
+    perror("execve");
+
+    for (int k = 0; envp_copy[k] != NULL; k++)
+        free(envp_copy[k]);
+    free(envp_copy);
+
+    return EXIT_FAILURE;
 }
